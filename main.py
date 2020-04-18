463ddfa5ca4ab0191373e19239d8b9934d3789db
Eldar Abraham <eldar.a@campus.technion.ac.il>
2020-04-18 13:15:48 +0300

    changed main to run on train1.wtag

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from log_linear_memm import Log_Linear_MEMM
 
 if __name__ == '__main__':
-    train_data = 'data/debugging_dataset.wtag'
+    train_data = 'data/train1.wtag'
     model = Log_Linear_MEMM()
     model.fit(train_path=train_data, threshold=10, lam=1)
     prediction = model.predict("Hadar went to the mall and bought some eggs .")
