99555d8b1bc27a66da0c0816b9d489ea86de2bd2
Hadar <hadarsi@campus.technion.ac.il>
2020-04-24 15:14:36 +0300

    actually fixed the code

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ if __name__ == '__main__':
     train_data = 'data/debugging_dataset.wtag'
     model = Log_Linear_MEMM()
     model.set_train_path(train_data)
-    model.preprocess(threshold=1)
+    model.preprocess(threshold=10)
     model.optimize(lam=1, maxiter=100)
     # model.fit(train_path=train_data, threshold=10, lam=1)
-    prediction = model.predict("Hadar went to the mall and bought some eggs .")
+    # prediction = model.predict("Hadar went to the mall and bought some eggs .")
+    prediction = model.predict(train_data)
     print(prediction)
