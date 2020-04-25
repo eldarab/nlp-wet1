c287a55bb7fd7cebd82e88f514b519c33463a70f
Eldar Abraham <eldar.a@campus.technion.ac.il>
2020-04-25 14:00:06 +0300

    created .gitignore
    So every time we run the main we dont update repository for nothing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from log_linear_memm import Log_Linear_MEMM
+from time import strftime
 
 
 def clean_predictions(input_data):
@@ -13,12 +14,14 @@ def clean_predictions(input_data):
 
 
 if __name__ == '__main__':
-    train_data = 'data/debugging_dataset_200.wtag'
+    time = strftime("%Y-%m-%d---%H-%M")
+    train_data = 'data/debugging_dataset_1.wtag'
     model = Log_Linear_MEMM()
     model.set_train_path(train_data)
     model.preprocess(threshold=10, f100=True, f101=False, f102=False, f103=True, f104=True, f105=True,
                      f108=False, f109=False, f110=False)
-    model.optimize(lam=1, maxiter=10, weights_path='dumps/weights_25-04-2020-1330.pkl')
-    prediction = model.predict('The Treasury is still working out the details with bank trade associations and the other government agencies that have a hand in fighting money laundering .')
+    model.optimize(lam=1, maxiter=1, weights_path='dumps/weights_' + time + '.pkl')
+    # prediction = model.predict('The Treasury is still working out the details with bank trade associations and the other government agencies that have a hand in fighting money laundering .')
+    prediction = model.predict('The Treasury is still working the 64-day .')
     print(prediction)
 
