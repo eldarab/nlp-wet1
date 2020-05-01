4d57e565d115e1488aed95850254f86e78eed6f6
Eldar Abraham <eldar.a@campus.technion.ac.il>
2020-05-01 18:26:54 +0300

    changed main to optimize overnight

--- log_linear_memm.py
+++ log_linear_memm.py
@@ -81,6 +81,7 @@ class Log_Linear_MEMM:
             f.write('train_path = ' + str(self.train_path) + '\n')
             f.write('dim = ' + str(self.dim) + '\n')
             f.write('threshold = ' + str(self.threshold) + '\n')
+            f.write('fix_threshold = ' + str(self.fix_threshold) + '\n')
             f.write('lam = ' + str(self.lam) + '\n')
             f.write('maxiter = ' + str(self.maxiter) + '\n')
             f.write('f100 = ' + str(self.f100) + '\n')
--- main.py
+++ main.py
@@ -2,12 +2,32 @@ from log_linear_memm import Log_Linear_MEMM
 from metodot_ezer import *
 
 if __name__ == '__main__':
-    model = Log_Linear_MEMM(threshold=10, lam=0, maxiter=1, f101=False, f102=False)
-    model.fit('data/debugging_dataset_200.wtag')
-    model.save()
-    model = load_model('dumps/test1.pkl')
-    test_path = 'data/debugging_dataset_201_210.wtag'
-    predictions = model.predict(test_path, beam_size=1)
-    accuracy = Log_Linear_MEMM.accuracy(test_path, predictions)
-    print(accuracy)
-    Log_Linear_MEMM.confusion_matrix(test_path, predictions)
+    print('training model1')
+    model1 = Log_Linear_MEMM(threshold=10, lam=0, maxiter=100, f101=False, f102=False)
+    model1.fit('data/train1.wtag')
+    model1.save('model1')
+
+    print('training model2')
+    model2 = Log_Linear_MEMM(threshold=10, lam=0, maxiter=100, fix_threshold=100)
+    model2.fit('data/train1.wtag')
+    model2.save('model2')
+
+    print('training model3')
+    model3 = Log_Linear_MEMM(threshold=10, lam=0.1, maxiter=100, fix_threshold=100)
+    model3.fit('data/train1.wtag')
+    model3.save('model3')
+
+    print('training model4')
+    model4 = Log_Linear_MEMM(threshold=10, lam=1, maxiter=100, fix_threshold=100)
+    model4.fit('data/train1.wtag')
+    model4.save('model4')
+
+    print('training model5')
+    model5 = Log_Linear_MEMM(threshold=10, lam=10, maxiter=100, fix_threshold=100)
+    model5.fit('data/train1.wtag')
+    model5.save('model5')
+
+    print('training model6')
+    model6 = Log_Linear_MEMM(threshold=10, lam=100, maxiter=100, fix_threshold=100)
+    model6.fit('data/train1.wtag')
+    model6.save('model6')
