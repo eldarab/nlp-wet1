8c9fa08a952427b03ef2f13babfcc13ab4b45bd3
Eldar Abraham <eldar.a@campus.technion.ac.il>
2020-05-02 22:33:28 +0300

    final main for CV

--- main.py
+++ main.py
@@ -11,27 +11,27 @@ def evaluate(train_path, model_index, threshold, fix_threshold, lam):
     model.save('model' + str(model_index))
 
     # predicting and saving predictions
-    predictions_test1 = model.predict('data/debugging_dataset_201_210.wtag', beam_size=2)  # TODO change test1.wtag
+    predictions_test1 = model.predict('data/test1.wtag', beam_size=2)  # test1.wtag
     with open('dumps/model' + str(model_index) + '_test1_predictions.pkl', 'wb') as f:
         pickle.dump(predictions_test1, f)
-    predictions_comp1 = model.predict('data/debugging_dataset_201_210.wtag', beam_size=2)  # TODO change comp1_nltk_tagged.wtag
+    predictions_comp1 = model.predict('data/comp1_nltk_tagged.wtag', beam_size=2)  # comp1_nltk_tagged.wtag
     with open('dumps/model' + str(model_index) + '_comp1_predictions.pkl', 'wb') as f:
         pickle.dump(predictions_comp1, f)
 
     # returning accuracy on test1.wtag and comp1_nltk_tagged.wtag
-    accuracy_test1 = Log_Linear_MEMM.accuracy('data/debugging_dataset_201_210.wtag', predictions_test1)  # TODO change test1.wtag
-    accuracy_comp1_tagged = Log_Linear_MEMM.accuracy('data/debugging_dataset_201_210.wtag', predictions_comp1)  # TODO change comp1_nltk_tagged.wtag
+    accuracy_test1 = Log_Linear_MEMM.accuracy('data/test1.wtag', predictions_test1)  # test1.wtag
+    accuracy_comp1_tagged = Log_Linear_MEMM.accuracy('data/comp1_nltk_tagged.wtag', predictions_comp1)  # comp1_XX.wtag
     return accuracy_test1, accuracy_comp1_tagged
 
 
 if __name__ == '__main__':
-    train_path = 'data/debugging_dataset_20.wtag'  # TODO change train1.wtag
-    model_index = 50  # starting from big number to make sure we dont confuse with previously trained models
+    train_path = 'data/train1.wtag'  # train1.wtag
+    model_index = 100  # starting from big number to make sure we dont confuse with previously trained models
     with open('dumps/report_02-05-2020.txt', 'w') as report:
         report.write('---CV Report 02-05-2020---\n\n')
     thresholds = [5, 10, 15, 20]
     fix_thresholds = [100, 200, 250]
-    lambdas = [0.1, 0.5, 0.7]  # TODO choose better lambdas
+    lambdas = [0.1, 0.5, 0.7]
     for threshold in thresholds:
         for fix_threshold in fix_thresholds:
             for lam in lambdas:
@@ -46,7 +46,7 @@ if __name__ == '__main__':
                 model_index += 1
                 print('Finished evaluating model index: ' + str(model_index))
 
-    # model = load_model('dumps/model3.pkl')
+
     # f102_dict = model.feature_statistics.f102_count_dict
     # f102_dict_sorted = {k: v for k, v in sorted(f102_dict.items(), key=lambda item: item[1], reverse=True)}
     # pass
